15bd821cbbc979fe6b6544c691c0d53448d48e80
jotkafomat <k-j@xl.wp.pl>
2021-05-19 15:04:24 +0100

    Initial Commit

---
+++ RSP/RSPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSPApp.swift
+//  RSP
+//
+//  Created by Krzysztof Jankowski on 19/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct RSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
